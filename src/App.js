9d23b5c355d32c25c95b695b0915ccdf9b8b0315
Chinedu Abalogu <chineduabalogu@gmail.com>
2022-10-06 18:51:01 +0100

    filter method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+const DATA = [
+  {
+    name: "Chinedu Prince Abalogu",
+    title: "Full stack software engineer",
+  },
+  {
+    name: "John Doe",
+    title: "Data Analyst",
+  },
+  {
+    name: "Joshua Lundar",
+    title: "Head of Engineering",
+  },
+  {
+    name: "Prince Bruew",
+    title: "Head of Technology",
+  },
+  {
+    name: "Isaac Einten",
+    title: "Integrations Lead",
+  },
+];
+
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  const [filteredData, setFilteredData] = useState(DATA);
+
+  useEffect(() => {
+    if (searchValue) {
+      const filtered = DATA.filter(({ name, title }) => {
+        return (
+          name.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()) ||
+          title.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())
+        );
+      });
+      setFilteredData(filtered);
+    } else {
+      setFilteredData(DATA);
+    }
+  }, [searchValue]);
+
   return (
     <div className="App">
-      <input type="text" placeholder="Search" />
+      <input
+        className="App-input"
+        type="text"
+        placeholder="Search"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+
+      {filteredData.map((data, idx) => {
+        return (
+          <div className="App-card">
+            <p className="App-card-name">{data.name}</p>
+            <p className="App-card-title">{data.title}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
